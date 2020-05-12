59b5d239820b828cc762026f9815eb840f72a7a8
sushma-sri <60297795+sushma-sri@users.noreply.github.com>
2020-05-12 15:36:48 +0530

    sushma to sushmasri

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,7 +5,7 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-dockerpath=sushma/ml-api:latest
+dockerpath=sushmasri/ml-api:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
